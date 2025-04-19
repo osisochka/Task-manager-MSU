89214c8f7668846a52f7e553b5e3645f7ab94b13
wdsjk <mamaevivan543@gmail.com>
2025-04-19 13:34:14 +0300

    domain

---
+++ src/main/java/org/example/weare8eqd/domain/Task.java
@@ -0,0 +1,37 @@
+package org.example.weare8eqd.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Table(name = "tasks")
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "task_id", nullable = false)
+    private Integer taskId;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String description;
+
+    @Column(nullable = false)
+    private LocalDateTime deadline;
+
+    private boolean started;
+
+    private boolean finished;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}

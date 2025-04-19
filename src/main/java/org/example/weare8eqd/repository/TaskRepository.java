1547c7179105d2e17bfa8a84b3adb5427f1e5aba
wdsjk <mamaevivan543@gmail.com>
2025-04-19 13:34:14 +0300

    domain

--- src/main/java/org/example/weare8eqd/repository/TaskRepository.java
+++ src/main/java/org/example/weare8eqd/repository/TaskRepository.java
@@ -1,5 +1,9 @@
 package org.example.weare8eqd.repository;
 
-public class TaskRepository {
+import org.example.weare8eqd.domain.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Integer> {
 }

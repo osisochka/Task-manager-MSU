9d9af856eabbb96891b7e9b1a63dd1c88bcabed6
Karpov Denis <d.karpov@edu.centraluniversity.ru>
2025-04-19 14:17:00 +0300

    Services + editing User

--- src/main/java/org/example/weare8eqd/domain/Task.java
+++ src/main/java/org/example/weare8eqd/domain/Task.java
@@ -1,9 +1,7 @@
 package org.example.weare8eqd.domain;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
@@ -12,6 +10,8 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Setter
+@Getter
 public class Task {
 
     @Id
